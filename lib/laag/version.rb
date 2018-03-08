73a557b142dc44bc41b606a1a0b0c4b16ca2322d
Chris Olstrom <chris@olstrom.com>
2018-03-08 15:44:52 -0800

    Improves Regexp for VERSION

--- lib/laag/version.rb
+++ lib/laag/version.rb
@@ -3,7 +3,7 @@
 
 module LAAG
   VERSION = $LOADED_FEATURES
-              .map { |f| f.match %r{laag-(?<version>[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(\.pre)?)} }
+              .map { |f| f.match %r{/laag-(?<version>[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+(\.pre)?)} }
               .compact
               .map { |gem| gem['version'] }
               .uniq
